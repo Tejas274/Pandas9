b70e4f29c52e3ece3145c6249132690116344360
tejas274 <shahtejas274@gmail.com>
2025-06-20 11:16:41 -0500

    removed problem 3

--- problem3-1050-actors-and-directors.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
-
-    df = actor_director.groupby(["actor_id","director_id"]).count().reset_index()
-    df = df[df['timestamp']==3]
-    return df[['actor_id','director_id']]
